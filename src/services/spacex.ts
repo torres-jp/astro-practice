a8ecab9941ab4216b0ccfe1ff8906921cbdf3231
torres-jp <torres.jpw@gmail.com>
2025-02-04 17:49:48 -0300

    add feching

---
+++ src/services/spacex.ts
@@ -0,0 +1,32 @@
+import { type Doc, type APISpaceX } from '../types/api'
+
+
+export const getLaunchesBy = async ({ id }: {id: string}) => {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+    const launch = (await res.json()) as Doc
+
+    console.log(launch)
+    return launch
+}
+
+
+export const getLaunches = async () => {
+
+    const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                    date: 'asc',
+                },
+                limit: 12,
+            },
+        }),
+    })
+    const { docs: launches } = (await res.json()) as APISpaceX
+    return launches
+}
